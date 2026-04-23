fd7c486eb55ea74afe437b2f0292f04315a60ea2
bhumi <wadhwani100188@gmail.com>
2026-04-23 11:50:03 +0530

    added fronted changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: '/api' })
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || '/api'
+})
 
-export const predictChurn   = (data)   => API.post('/predict/', data)
-export const getAnalytics   = ()       => API.get('/analytics/summary')
+export const predictChurn = (data) => API.post('/predict/', data)
+export const getAnalytics = ()     => API.get('/analytics/summary')
